5dba5abb9a4827c02fa532d533cffeaafea01b82
yuning.lv <lvyuning.banben@foxmail.com>
2020-08-15 11:05:55 +0800

    重新提交github-page的build路径配置

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '', // github-pages要空，build的index.html要./同级路径才能展示
 
     /**
      * Source Maps
