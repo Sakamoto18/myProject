9d131344cd150f7c8b0a6eac5a425c6862d62164
yuning.lv <lvyuning.banben@foxmail.com>
2020-08-17 18:08:30 +0800

    时间线动画溢出修改，B站二维码添加

--- src/components/timeLine/timeLine.vue
+++ src/components/timeLine/timeLine.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="timeline-small absolute">
         <div class="timeline-small-body">
-            <div class="year-bubble pointer relative ml-16 h-30 bold flex absolute-center mt-4">
+            <div id="bubble" class="year-bubble pointer relative ml-16 h-30 bold flex absolute-center mt-4">
                 <span v-if="year">{{ year }}</span>
-                <svg-icon v-else slot="reference" icon-class="timeExperience" class="icon-space pointer h-40 mt-1"></svg-icon>
+                <svg-icon v-else slot="reference" icon-class="timeLine" class="icon-space pointer h-40 ml-1 mt-1"></svg-icon>
             </div>
             <ul>
                 <li class="pointer" @mouseover="changeYears(index)" @mouseleave="rollBackStyle(index)" v-for="(item, index) in experienseList" :key="index">
@@ -40,10 +40,13 @@ export default {
                 this.year = this.experienseList[index].date.split('年')[0]
                 document.getElementById(`bullet${index}`).style.border = '5px solid #A6DB6D'
                 document.getElementById(`bullet${index}`).style.transition = 'border 0.2s'
+                document.getElementById('bubble').style.textShadow = '2px 2px 2px rgba(0,0,0,0.5)'
+                document.getElementById('bubble').style.transition = 'text-shadow 0.5s'
             })
         },
         rollBackStyle(index) {
             document.getElementById(`bullet${index}`).style.border = '3px solid #A6DB6D'
+            document.getElementById('bubble').style.textShadow = ''
         }
     }
 }
@@ -54,22 +57,20 @@ export default {
     border-radius: 50%;
     width: 100px;
     height: 100px;
-    margin: auto;
     background: no-repeat 50%/70% #eee;
     color: #69ca62;
-    box-shadow: 0 0 0 6px rgba(105, 202, 98, 0.5); /* inset 为内阴影，设置后阴影方向在边框内部 */
+    box-shadow: inset 0 0 0 5px rgba(105, 202, 98, 0.5); /* inset 为内阴影，设置后阴影方向在边框内部 */
 
 }
 .year-bubble::before, .year-bubble::after {
     position: absolute;
-    border-radius: 50%;
     top: 0; bottom: 0; left: 0; right: 0;
 }
 /*定义两个伪类的样式及动画开始*/
 .year-bubble::before, .year-bubble::after {
     content: '';
     z-index: -1;
-    margin: -5%;
+    margin: -3%;
     border-radius: 50%;
     box-shadow: 0 0 0 2px;
     animation: clipMe 8s linear infinite;
@@ -99,10 +100,10 @@ export default {
 // }
 @keyframes clipMe {
     0%, 100% {
-        clip: rect(0px, 220.0px, 2px, 0px); /* 描边 */
+        clip: rect(-4px, 220.0px, 2px, -4px); /* 描边 */
     }
     25% {
-        clip: rect(0px, 2px, 220.0px, 0px);
+        clip: rect(-4px, 2px, 220.0px, -4px); /* 防止遮挡，定位边框位置为-4px，不然会出现断层 */
     }
     50% {
         clip: rect(218.0px, 220.0px, 220.0px, 0px);
