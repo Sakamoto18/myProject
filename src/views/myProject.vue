4f6029882d83e99c06dbc262d83ca1568bccdf22
yuning.lv <lvyuning.banben@foxmail.com>
2022-03-21 18:52:08 +0800

    feat: 2022-3-21 change index bg

--- src/views/myProject.vue
+++ src/views/myProject.vue
@@ -36,7 +36,7 @@
                     <div v-if="item === '1'">
                         <bubble-flow>
                                 <div slot="card" class="name-card animation-0 flex absolute-center relative" :class="{shadow: ifMouseMove}">
-                                    <div class="h-70 font-family-kai color-white bold full cursor-none">我叫吕宇宁</div>
+                                    <div class="h-70 font-family-kai color-white bold full cursor-none">我是吕宇宁</div>
                                     <div class="h-20 font-family-roman color-white full cursor-none">Love Code. Show Code</div>
                                     <div class="h-20 font-family-kai color-white full cursor-none">一名前端开发工程师</div>
                                     <div class="h-20 color-white font-family-roman full">
